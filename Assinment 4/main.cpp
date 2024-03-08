1ed81acfc0d00c33530c6ebdf9f08782c443cde6
Duha Iqbal <DIqbal0519@gmail.com>
2024-03-07 20:02:18 -0500

    1

--- Assinment 4/main.cpp
+++ Assinment 4/main.cpp
@@ -1,14 +1,45 @@
-//
-//  main.cpp
-//  Assinment 4
-//
-//  Created by Duha Iqbal on 3/7/24.
-//
+/*
+ COP3014: Programmming II
+ Assignment 4 - creating a program that randomly generates random score up between 1-20 for up to 10 quizzes,this is the imput file. Then the program calculates the average and adds it to the output file.
+ Duha Iqbal
+ 02/23/2024
+*/
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class COP3014{
+private:
+    string first_name;
+    string last_name;
+    int Z_number;
+    int grades;
+    double total_grade;
+    char final_letter_grade;
+public:
+    //constructors
+    COP3014(string first_name, string last_name, int Z_number, int grades);
+    COP3014(string first_name, string last_name, int Z_number);
+    COP3014(string first_name, string last_name);
+    COP3014(string first_name);
+    COP3014();
+    
+    //setters
+    void set(string first_name, string last_name, int Z_number, int grades);
+    void set(string first_name, string last_name, int Z_number);
+    void set(string first_name, string last_name);
+    void set(string first_name);
+    void set();
+    
+    //input function that asks the user for student's name, Z-number and grades
+    void input(string& first_name, string& last_name, int& Z_number, int& grades);
+    
+    //computes the student’s total grade
+    
+};
+
+
+int main() {
+    cout << "Hello, World!\n";
     return 0;
 }
