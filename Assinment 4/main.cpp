672fdfca4ba6fb7acdb3f9ce948a3858357f7b1f
Diqbal0519 <157728954+Diqbal0519@users.noreply.github.com>
2024-03-15 22:11:40 -0400

    Update main.cpp

--- Assinment 4/main.cpp
+++ Assinment 4/main.cpp
@@ -1,10 +1,3 @@
-/*
- COP3014: Programmming II
- Assignment 4 - creating a program that randomly generates random score up between 1-20 for up to 10 quizzes,this is the imput file. Then the program calculates the average and adds it to the output file.
- Duha Iqbal
- 02/23/2024
-*/
-
 #include <iostream>
 using namespace std;
 
@@ -20,26 +13,26 @@ private:
     double midtermGrade;
     double finalGrade;
     double totalGrade;
-    char letterGrade;
+    string letterGrade;
 public:
     //constructors
-    COP3014(string first_name, string last_name, int Z_number);
-    COP3014(string first_name, string last_name);
-    COP3014(int Z_number);
+    COP3014(string user_first, string user_last, int user_Znumber);
+    COP3014(string user_first, string user_last);
+    COP3014(int user_Znumber);
     COP3014();
     
     //setters
-    void set_first_name(string first_name);
-    void set_last_name(string last_name);
-    void set_Znumber(int Z_number);
-    void set_assign_1_grade(double assign_1_grade);
-    void set_assign_2_grade(double assign_2_grade);
-    void set_assign_3_grade(double assign_3_grade);
-    void set_assign_4_grade(double assign_4_grade);
-    void set_midterm_grade(double midterm_grade);
-    void set_final_grade(double final_grade);
-    void set_total_grade(double total_grade);
-    void set_letter_grade(char letter_grade);
+    void set_first_name(string user_first);
+    void set_last_name(string user_last);
+    void set_Znumber(int user_Znumber);
+    void set_assign_1_grade(double user_1_grade);
+    void set_assign_2_grade(double user_2_grade);
+    void set_assign_3_grade(double user_3_grade);
+    void set_assign_4_grade(double user_4_grade);
+    void set_midterm_grade(double user_midterm_grade);
+    void set_final_grade(double user_final_grade);
+    void set_total_grade(double user_total_grade);
+    void set_letter_grade(string user_letter_grade);
     
     //getters
     string get_first_name();
@@ -52,50 +45,86 @@ public:
     double get_midterm_grade();
     double get_final_grade();
     double get_total_grade();
-    char get_letter_grade();
+    string get_letter_grade();
     
     //input function that asks the user for student's name, Z-number and grades
     void input();
     
     //computes the student’s total grade
-    void total_grade(double assinmentOneGrade, double assinmentTwoGrade, double assinmentThreeGrade, double assinmentFourGrade, double midtermGrade, double finalGrade);
+    void total_grade();
     
     //computes the student’s final letter grade
-    void final_grade(double totalGrade, char& letterGrade);
+    void final_letter_grade(double totalGrade);
+    
+    //checks midterm and final grade
+    void check_grades();
     
     //output function which outputs information of any student
-    void output();
+    void output(COP3014 temp);
 };
 
 class Spring24: public COP3014{
 private:
+    string firstName = COP3014::get_first_name();
+    string lastName = COP3014::get_last_name();
+    int ZNumber = COP3014::get_Znumber();
+    double assinmentOneGrade = COP3014::get_assign_1_grade();
+    double assinmentTwoGrade = COP3014::get_assign_2_grade();
+    double assinmentThreeGrade = COP3014::get_assign_3_grade();
+    double assinmentFourGrade = COP3014::get_assign_4_grade();
+    double midtermGrade = COP3014::get_midterm_grade();
+    double finalGrade = COP3014::get_final_grade();
+    double totalGrade = COP3014::get_total_grade();
+    string letterGrade = COP3014::get_letter_grade();
     int labGrade;
 public:
+    //constructors
+    Spring24(string user_first, string user_last, int user_Znumber);
+    Spring24(string user_first, string user_last);
+    Spring24(int user_Znumber);
+    Spring24();
+    
     //setter
     void set_lab_grade(double lab_grade);
+    
+    //getter
+    double get_lab_grade();
     
     //input function that asks the user for student's name, Z-number, and grades, including lab grade
     void input_with_lab();
     
-    //computes the student’s total grade
-    void totalGrade(double assinmentOneGrade, double assinmentTwoGrade, double assinmentThreeGrade, double assinmentFourGrade, double midtermGrade, double finalGrade, int lab_grade, double& totalGrade);
+    //computes the student’s total grade, including lab grade
+    void total_grade();
     
-    //output function which outputs information of any student
-    void output(int Z_number, int grades, int lab_grade);
+    //output function which outputs information of any student, with lab grade
+    void output_with_lab(Spring24 temp);
 };
 
 
 int main() {
-    COP3014 test("Duha", "Iqbal");
-    cout << test.get_first_name() << endl;
-    
+    char repeat, type;
+    do{
+        cout << "Is the student apart of Spring 24?(y or n): ";
+        cin >> type;
+        if(type == 'N' || type == 'n'){
+            COP3014 student;
+            student.input();
+            student.output(student);
+        }else if(type == 'Y' || type == 'y'){
+            Spring24 student_lab;
+            student_lab.input_with_lab();
+            student_lab.output_with_lab(student_lab);
+        }
+        cout << "Do you want to calculate another students grade?(y or n): ";
+        cin >> repeat;
+    }while(repeat == 'Y' || repeat == 'y');
     return 0;
 }
 
-COP3014::COP3014(string first_name, string last_name, int Z_number){
-    firstName = first_name;
-    lastName = last_name;
-    ZNumber = Z_number;
+COP3014::COP3014(string user_first, string user_last, int user_Znumber){
+    firstName = user_first;
+    lastName = user_last;
+    ZNumber = user_Znumber;
     assinmentOneGrade = 100;
     assinmentTwoGrade = 100;
     assinmentThreeGrade = 100;
@@ -106,9 +135,9 @@ COP3014::COP3014(string first_name, string last_name, int Z_number){
     letterGrade = 'A';
 }
 
-COP3014::COP3014(string first_name, string last_name){
-    firstName = first_name;
-    lastName = last_name;
+COP3014::COP3014(string user_first, string user_last){
+    firstName = user_first;
+    lastName = user_last;
     ZNumber = 12345678;
     assinmentOneGrade = 100;
     assinmentTwoGrade = 100;
@@ -120,10 +149,10 @@ COP3014::COP3014(string first_name, string last_name){
     letterGrade = 'A';
 }
 
-COP3014::COP3014(int Z_number){
+COP3014::COP3014(int user_Znumber){
     firstName = "First";
     lastName = "Last";
-    ZNumber = Z_number;
+    ZNumber = user_Znumber;
     assinmentOneGrade = 100;
     assinmentTwoGrade = 100;
     assinmentThreeGrade = 100;
@@ -148,48 +177,48 @@ COP3014::COP3014(){
     letterGrade = 'A';
 }
 
-void COP3014::set_first_name(string first_name){
-    firstName = first_name;
+void COP3014::set_first_name(string user_first){
+    firstName = user_first;
 }
 
-void COP3014::set_last_name(string last_name){
-    lastName = last_name;
+void COP3014::set_last_name(string user_last){
+    lastName = user_last;
 }
 
-void COP3014::set_Znumber(int Z_number){
-    ZNumber = Z_number;
+void COP3014::set_Znumber(int user_Znumber){
+    ZNumber = user_Znumber;
 }
 
-void COP3014::set_assign_1_grade(double assign_1_grade){
-    assinmentOneGrade = assign_1_grade;
+void COP3014::set_assign_1_grade(double user_1_grade){
+    assinmentOneGrade = user_1_grade;
 }
 
-void COP3014::set_assign_2_grade(double assign_2_grade){
-    assinmentTwoGrade = assign_2_grade;
+void COP3014::set_assign_2_grade(double user_2_grade){
+    assinmentTwoGrade = user_2_grade;
 }
 
-void COP3014::set_assign_3_grade(double assign_3_grade){
-    assinmentThreeGrade = assign_3_grade;
+void COP3014::set_assign_3_grade(double user_3_grade){
+    assinmentThreeGrade = user_3_grade;
 }
 
-void COP3014::set_assign_4_grade(double assign_4_grade){
-    assinmentFourGrade = assign_4_grade;
+void COP3014::set_assign_4_grade(double user_4_grade){
+    assinmentFourGrade = user_4_grade;
 }
 
-void COP3014::set_midterm_grade(double midterm_grade){
-    midtermGrade = midterm_grade;
+void COP3014::set_midterm_grade(double user_midterm_grade){
+    midtermGrade = user_midterm_grade;
 }
 
-void COP3014::set_final_grade(double final_grade){
-    finalGrade = final_grade;
+void COP3014::set_final_grade(double user_final_grade){
+    finalGrade = user_final_grade;
 }
 
-void COP3014::set_total_grade(double total_grade){
-    totalGrade = total_grade;
+void COP3014::set_total_grade(double user_total_grade){
+    totalGrade = user_total_grade;
 }
 
-void COP3014::set_letter_grade(char letter_grade){
-    letterGrade = letter_grade;
+void COP3014::set_letter_grade(string user_letter_grade){
+    letterGrade = user_letter_grade;
 }
 
 string COP3014::get_first_name(){
@@ -232,7 +261,7 @@ double COP3014::get_total_grade(){
     return totalGrade;
 }
 
-char COP3014::get_letter_grade(){
+string COP3014::get_letter_grade(){
     return letterGrade;
 }
 
@@ -258,61 +287,208 @@ void COP3014:: input(){
     cout << "Enter assinment 4 grade: ";
     cin >> assinmentFourGrade;
     COP3014::set_assign_4_grade(assinmentFourGrade);
-    cout << "Enter midterm grade: ";
+    cout << "Enter midterm grade(if absent/missed enter 0): ";
     cin >> midtermGrade;
-    COP3014::set_final_grade(midtermGrade);
-    cout << "Enter final grade: ";
+    COP3014::set_midterm_grade(midtermGrade);
+    cout << "Enter final grade(if absent/missed enter 0): ";
     cin >> finalGrade;
     COP3014::set_final_grade(finalGrade);
 }
 
-void COP3014::total_grade(double assinmentOneGrade, double assinmentTwoGrade, double assinmentThreeGrade, double assinmentFourGrade, double midtermGrade, double finalGrade){
+void COP3014::total_grade(){
     double total;
     total = ((assinmentOneGrade + assinmentTwoGrade +assinmentThreeGrade + assinmentFourGrade)/4.0) * 0.25 + midtermGrade *0.3 + finalGrade * 0.45;
     COP3014::set_total_grade(total);
+}
+
+void COP3014::final_letter_grade(double totalGrade){
+    midtermGrade = get_midterm_grade();
+    finalGrade = get_final_grade();
     
-}
-
-void COP3014::final_grade(double totalGrade, char& letterGrade){
-    if(totalGrade = 100 && totalGrade >= 90){
-        COP3014::set_letter_grade('A');
-    }
-    else if(totalGrade < 90  && totalGrade >= 87){
-        COP3014::set_letter_grade('A-');
-    }
-    else if(totalGrade < 87  && totalGrade >= 83){
-        COP3014::set_letter_grade('B+');
-    }
-    else if(totalGrade < 83  && totalGrade >= 80){
-        COP3014::set_letter_grade('B');
-    }
-    else if(totalGrade < 80  && totalGrade >= 77){
-        COP3014::set_letter_grade('B-');
-    }
-    else if(totalGrade < 77  && totalGrade >= 73){
-        COP3014::set_letter_grade('C+');
-    }
-    else if(totalGrade < 73  && totalGrade >= 70){
-        COP3014::set_letter_grade('C');
-    }
-    else if(totalGrade < 70  && totalGrade >= 67){
-        COP3014::set_letter_grade('C-');
-    }
-    else if(totalGrade < 67  && totalGrade >= 63){
-        COP3014::set_letter_grade('D+');
-    }
-    else if(totalGrade < 63  && totalGrade >= 60){
-        COP3014::set_letter_grade('D');
-    }
-    else if(totalGrade < 60  && totalGrade >= 51){
-        COP3014::set_letter_grade('D-');
-    }
-    else if(totalGrade < 60  && totalGrade >= 0){
-        COP3014::set_letter_grade('F');
+    if (totalGrade >= 90) {
+        COP3014::set_letter_grade("A");
+    } else if (totalGrade >= 87) {
+        COP3014::set_letter_grade("A-");
+    } else if (totalGrade >= 83) {
+        COP3014::set_letter_grade("B+");
+    } else if (totalGrade >= 80) {
+        COP3014::set_letter_grade("B");
+    } else if (totalGrade >= 77) {
+       COP3014::set_letter_grade("B-");
+    } else if (totalGrade >= 73) {
+        COP3014::set_letter_grade("C+");
+    } else if (totalGrade >= 70) {
+        COP3014::set_letter_grade("C");
+    } else if (totalGrade >= 67) {
+       COP3014::set_letter_grade("C-");
+    } else if (totalGrade >= 63) {
+        COP3014::set_letter_grade("D+");
+    } else if (totalGrade >= 60) {
+       COP3014::set_letter_grade("D");
+    } else if (totalGrade >= 51) {
+        COP3014::set_letter_grade("D-");
+    } else {
+        COP3014::set_letter_grade("F");
     }
 }
 
+void COP3014::check_grades(){
+  if (midtermGrade == 0 && totalGrade >= 70) {
+        COP3014::set_letter_grade("C");
+    } else if (midtermGrade == 0 && totalGrade >= 67) {
+        COP3014::set_letter_grade("C-");
+    } else if (midtermGrade == 0 && totalGrade >= 63) {
+        COP3014::set_letter_grade("D+");
+    } else if (midtermGrade == 0 && totalGrade >= 60) {
+        COP3014::set_letter_grade("D");
+    } else if (midtermGrade == 0 && totalGrade >= 51) {
+        COP3014::set_letter_grade("D-");
+    } else if(finalGrade == 0){
+        COP3014::set_letter_grade("F");
+    } else {
+        get_letter_grade();
+    }
+}
 
-void COP3014::output(){
+void COP3014::output(COP3014 temp){
+    temp.total_grade();
+    double totalGrade = temp.get_total_grade();
+    temp.final_letter_grade(totalGrade);
+    temp.check_grades();
     
+    cout << endl << "COP3014" << endl;
+    cout << temp.get_first_name() << " " << temp.get_last_name() << "-" << "Z" << temp.get_Znumber() << endl;
+    cout << "Assign 1: " << temp.get_assign_1_grade() << endl;
+    cout << "Assign 2: " << temp.get_assign_2_grade() << endl;
+    cout << "Assign 3: " << temp.get_assign_3_grade() << endl;
+    cout << "Assign 4: " << temp.get_assign_4_grade() << endl;
+    cout << "Midterm: " << temp.get_midterm_grade() << endl;
+    cout << "Final: " << temp.get_final_grade() << endl;
+    cout << "Total grade: " << temp.get_total_grade() << endl;
+    cout << "Final grade: " << temp.get_letter_grade() << endl;
 }
+
+Spring24::Spring24(string user_first, string user_last, int user_Znumber){
+    set_first_name(user_first);
+    set_last_name(user_last);
+    set_Znumber(user_Znumber);
+    set_assign_1_grade(100);
+    set_assign_2_grade(100);
+    set_assign_3_grade(100);
+    set_assign_4_grade(100);
+    set_midterm_grade(100);
+    set_final_grade(100);
+    labGrade = 100;
+    set_total_grade(100);
+    set_letter_grade("A");
+}
+
+Spring24::Spring24(string user_first, string user_last){
+    set_first_name(user_first);
+    set_last_name(user_last);
+    set_Znumber(12345678);
+    set_assign_1_grade(100);
+    set_assign_2_grade(100);
+    set_assign_3_grade(100);
+    set_assign_4_grade(100);
+    set_midterm_grade(100);
+    set_final_grade(100);
+    labGrade = 100;
+    set_total_grade(100);
+    set_letter_grade("A");
+}
+
+Spring24::Spring24(int user_Znumber){
+    set_first_name("First");
+    set_last_name("Last");
+    set_Znumber(user_Znumber);
+    set_assign_1_grade(100);
+    set_assign_2_grade(100);
+    set_assign_3_grade(100);
+    set_assign_4_grade(100);
+    set_midterm_grade(100);
+    set_final_grade(100);
+    labGrade = 100;
+    set_total_grade(100);
+    set_letter_grade("A");
+}
+
+Spring24::Spring24(){
+    set_first_name("First");
+    set_last_name("Last");
+    set_Znumber(12345678);
+    set_assign_1_grade(100);
+    set_assign_2_grade(100);
+    set_assign_3_grade(100);
+    set_assign_4_grade(100);
+    set_midterm_grade(100);
+    set_final_grade(100);
+    labGrade = 100;
+    set_total_grade(100);
+    set_letter_grade("A");
+}
+
+void Spring24::set_lab_grade(double lab_grade){
+    labGrade = lab_grade;
+}
+
+double Spring24::get_lab_grade(){
+    return labGrade;
+}
+
+void Spring24::input_with_lab(){
+    cout << "Enter first name: ";
+    cin >> firstName;
+    set_first_name(firstName);
+    cout << "Enter last name: ";
+    cin >> lastName;
+    Spring24::set_last_name(lastName);
+    cout << "Enter Z number(without the Z): ";
+    cin >> ZNumber;
+    Spring24::set_Znumber(ZNumber);
+    cout << "Enter assinment 1 grade: ";
+    cin >> assinmentOneGrade;
+    Spring24::set_assign_1_grade(assinmentOneGrade);
+    cout << "Enter assinment 2 grade: ";
+    cin >> assinmentTwoGrade;
+    Spring24::set_assign_2_grade(assinmentTwoGrade);
+    cout << "Enter assinment 3 grade: ";
+    cin >> assinmentThreeGrade;
+    Spring24::set_assign_3_grade(assinmentThreeGrade);
+    cout << "Enter assinment 4 grade: ";
+    cin >> assinmentFourGrade;
+    Spring24::set_assign_4_grade(assinmentFourGrade);
+    cout << "Enter lab grade: ";
+    cin >> labGrade;
+    Spring24::set_lab_grade(labGrade);
+    cout << "Enter midterm grade: ";
+    cin >> midtermGrade;
+    Spring24::set_midterm_grade(midtermGrade);
+    cout << "Enter final grade: ";
+    cin >> finalGrade;
+    Spring24::set_final_grade(finalGrade);
+ }
+ 
+ void Spring24::total_grade(){
+     double total;
+     total = ((assinmentOneGrade + assinmentTwoGrade +assinmentThreeGrade + assinmentFourGrade)/4.0) * 0.25 + midtermGrade *0.25 + finalGrade * 0.4 + labGrade *0.1;
+     Spring24::set_total_grade(total);
+ }
+ 
+ void Spring24::output_with_lab(Spring24 temp){
+    temp.total_grade();
+    double totalGrade = temp.get_total_grade();
+    temp.final_letter_grade(totalGrade);
+    temp.check_grades();
+    
+    cout << endl << "COP3014" << endl;
+    cout << temp.get_first_name() << " " << temp.get_last_name() << "-" << "Z" << temp.get_Znumber() << endl;
+    cout << "Assign 1: " << temp.get_assign_1_grade() << endl;
+    cout << "Assign 2: " << temp.get_assign_2_grade() << endl;
+    cout << "Assign 3: " << temp.get_assign_3_grade() << endl;
+    cout << "Assign 4: " << temp.get_assign_4_grade() << endl;
+    cout << "Midterm: " << temp.get_midterm_grade() << endl;
+    cout << "Final: " << temp.get_final_grade() << endl;
+    cout << "Total grade: " << temp.get_total_grade() << endl;
+    cout << "Final grade: " << temp.get_letter_grade() << endl;
+ }
